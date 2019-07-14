5225f775ac380b9dc06e206fb5fc6f026c0f2fba
szpila127 <inglot.seb@gmail.com>
2019-07-15 00:41:34 +0200

    1

---
+++ src/main/java/com/kodilla/tictactoe/Symbol.java
@@ -0,0 +1,6 @@
+package com.kodilla.tictactoe;
+
+public class Symbol {
+    private Symbol cross;
+    private Symbol circle;
+}
--- src/main/java/com/kodilla/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/kodilla/tictactoe/TicTacToeApplication.java
@@ -1,10 +1,13 @@
 package com.kodilla.tictactoe;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +15,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TicTacToeApplication extends Application {
 
+
     private Image imageback = new Image("file:src/main/resources/back.png");
+    private Image cross = new Image("file:src/main/resources/cross.png");
+    private Image circle = new Image("file:src/main/resources/circle.png");
+    private FlowPane symbols = new FlowPane(Orientation.HORIZONTAL);
 
     public static void main(String[] args) {
         launch(args);
@@ -25,10 +32,22 @@ public class TicTacToeApplication extends Application {
         Background background = new Background(backgroundImage);
 
         GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        grid.setHgap(5.5);
+        grid.setVgap(20.5);
+
         grid.setBackground(background);
 
+        ImageView img = new ImageView(cross);
+        ImageView img2 = new ImageView(circle);
+        symbols.getChildren().add(img);
+        symbols.getChildren().add(img2);
 
-        Scene scene = new Scene(grid, 606, 606, Color.BLACK);
+        grid.add(symbols, 0, 3, 3, 1);
+
+
+        Scene scene = new Scene(grid, 606, 606);
 
         primaryStage.setTitle("TicTacToe");
         primaryStage.setScene(scene);
