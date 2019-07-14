f5f20e6677d895715ffe06fa1545f6f6bba7ae3a
szpila127 <inglot.seb@gmail.com>
2019-07-14 22:35:17 +0200

    1

--- src/main/java/com/kodilla/tictactoe/TicTacToeApplication.java
+++ src/main/java/com/kodilla/tictactoe/TicTacToeApplication.java
@@ -3,8 +3,12 @@ package com.kodilla.tictactoe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+
 @SpringBootApplication
 public class TicTacToeApplication {
+
+    private Image imageback = new Image("file:resources/back.png");
 
     public static void main(String[] args) {
         SpringApplication.run(TicTacToeApplication.class, args);
