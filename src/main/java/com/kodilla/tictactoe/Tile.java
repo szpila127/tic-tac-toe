f639083c3d080d43de5bd3cfbf97982e4a82289a
szpila127 <inglot.seb@gmail.com>
2019-09-09 22:01:20 +0200

    Final

--- src/main/java/com/kodilla/tictactoe/Tile.java
+++ src/main/java/com/kodilla/tictactoe/Tile.java
@@ -8,6 +8,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 
+import java.util.Arrays;
+
 public class Tile extends StackPane {
 
     private static Image cross = new Image("file:src/main/resources/cross.png");
@@ -42,6 +44,9 @@ public class Tile extends StackPane {
             if (!TicTacToeApplication.possibleMove) {
                 return;
             }
+            else if (action.getButton() == MouseButton.SECONDARY) {
+                return;
+            }
             if (action.getButton() == MouseButton.PRIMARY) {
                 if (!TicTacToeApplication.turnX || getFieldValue() == FieldValue.CIRCLE || getFieldValue() == FieldValue.CROSS) {
                     return;
@@ -49,52 +54,69 @@ public class Tile extends StackPane {
                 setFieldValue(FieldValue.CROSS);
                 rectangle.setFill(new ImagePattern(cross));
                 TicTacToeApplication.turnX = false;
-
-            } else if (action.getButton() == MouseButton.SECONDARY) {
-                if (TicTacToeApplication.turnX || getFieldValue() == FieldValue.CROSS || getFieldValue() == FieldValue.CIRCLE) {
-                    return;
-                }
-                setFieldValue(FieldValue.CIRCLE);
-                rectangle.setFill(new ImagePattern(circle));
-                TicTacToeApplication.turnX = true;
+                checkState();
             }
-
-            Score score = State.gameResult();
-            System.out.println(score);
-            if (score == Score.O_WIN) {
-                TicTacToeApplication.playerO.setCount(TicTacToeApplication.playerO.getCount() + 1);
-                TicTacToeApplication.counter.setText("CROSS[" + TicTacToeApplication.playerX.getCount()
-                        + "] : [" + TicTacToeApplication.playerO.getCount() + "]CIRCLE");
-                TicTacToeApplication.possibleMove = false;
-                TicTacToeApplication.label.setText("WINNER: O");
-                TicTacToeApplication.label.setTextFill(Color.BLACK);
+            if (!TicTacToeApplication.possibleMove) {
                 return;
             }
-            if (score == Score.X_WIN) {
-                TicTacToeApplication.playerX.setCount(TicTacToeApplication.playerX.getCount() + 1);
-                TicTacToeApplication.counter.setText("CROSS[" + TicTacToeApplication.playerX.getCount()
-                        + "] : [" + TicTacToeApplication.playerO.getCount() + "]CIRCLE");
-                TicTacToeApplication.possibleMove = false;
-                TicTacToeApplication.label.setText("WINNER: X");
-                TicTacToeApplication.label.setTextFill(Color.RED);
-                return;
-            }
-            if (score == Score.EVEN) {
-                TicTacToeApplication.possibleMove = false;
-                TicTacToeApplication.label.setText("EVEN");
-                TicTacToeApplication.label.setTextFill(Color.GREEN);
-
-            }
-            if (score == Score.IN_PROGRESS) {
-                if (TicTacToeApplication.turnX) {
-                    TicTacToeApplication.label.setText("CROSS TURN");
-                    TicTacToeApplication.label.setTextFill(Color.RED);
-                }
-                if (!TicTacToeApplication.turnX) {
-                    TicTacToeApplication.label.setText("CIRCLE TURN");
-                    TicTacToeApplication.label.setTextFill(Color.BLACK);
-                }
-            }
+            Tile tile = Arrays.stream(TicTacToeApplication.board.fields)
+                    .flatMap(array -> Arrays.stream(array))
+                    .filter(field -> field.getFieldValue() == FieldValue.EMPTY)
+                    .findFirst()
+                    .get();
+            Rectangle node = (Rectangle) tile.getChildren().get(0);
+            tile.setFieldValue(FieldValue.CIRCLE);
+            node.setFill(new ImagePattern(circle));
+            TicTacToeApplication.turnX = true;
+            checkState();
+//            else if (action.getButton() == MouseButton.SECONDARY) {
+//                if (TicTacToeApplication.turnX || getFieldValue() == FieldValue.CROSS || getFieldValue() == FieldValue.CIRCLE) {
+//                    return;
+//                }
+//                setFieldValue(FieldValue.CIRCLE);
+//                rectangle.setFill(new ImagePattern(circle));
+//                TicTacToeApplication.turnX = true;
+//                checkState();
+//            }
         });
+    }
+
+    public void checkState() {
+        Score score = State.gameResult();
+        System.out.println(score);
+        if (score == Score.O_WIN) {
+            TicTacToeApplication.playerO.setCount(TicTacToeApplication.playerO.getCount() + 1);
+            TicTacToeApplication.counter.setText("CROSS[" + TicTacToeApplication.playerX.getCount()
+                    + "] : [" + TicTacToeApplication.playerO.getCount() + "]CIRCLE");
+            TicTacToeApplication.possibleMove = false;
+            TicTacToeApplication.label.setText("WINNER: O");
+            TicTacToeApplication.label.setTextFill(Color.BLACK);
+            return;
+        }
+        if (score == Score.X_WIN) {
+            TicTacToeApplication.playerX.setCount(TicTacToeApplication.playerX.getCount() + 1);
+            TicTacToeApplication.counter.setText("CROSS[" + TicTacToeApplication.playerX.getCount()
+                    + "] : [" + TicTacToeApplication.playerO.getCount() + "]CIRCLE");
+            TicTacToeApplication.possibleMove = false;
+            TicTacToeApplication.label.setText("WINNER: X");
+            TicTacToeApplication.label.setTextFill(Color.RED);
+            return;
+        }
+        if (score == Score.EVEN) {
+            TicTacToeApplication.possibleMove = false;
+            TicTacToeApplication.label.setText("EVEN");
+            TicTacToeApplication.label.setTextFill(Color.GREEN);
+            return;
+        }
+        if (score == Score.IN_PROGRESS) {
+            if (TicTacToeApplication.turnX) {
+                TicTacToeApplication.label.setText("CROSS TURN");
+                TicTacToeApplication.label.setTextFill(Color.RED);
+            }
+            if (!TicTacToeApplication.turnX) {
+                TicTacToeApplication.label.setText("CIRCLE TURN");
+                TicTacToeApplication.label.setTextFill(Color.BLACK);
+            }
+        }
     }
 }
